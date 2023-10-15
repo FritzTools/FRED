ca30c1d78f2cc14de22238a1882d5e414fbdf53d
Adrian Preuß <administrates@icloud.com>
2023-10-15 11:50:56 +0200

    Fix Exceptions on `Request` methods

--- Source/Core/Fritzbox/Networking/HTTP/Request.cs
+++ Source/Core/Fritzbox/Networking/HTTP/Request.cs
@@ -20,34 +20,43 @@ namespace FRED.Core.Fritzbox.Networking.HTTP {
         }
 
         private static async Task<String> PostAsync(String url, KeyValuePair<String, String>[] content) {
-            return await PostAsync(url, new FormUrlEncodedContent(content));
+            try {
+                return await PostAsync(url, new FormUrlEncodedContent(content));
+            } catch (Exception) {
+                return null;
+            }
         }
 
         private static async Task<String> PostAsync(String url, String content) {
-            return await PostAsync(url, new StringContent(content, Encoding.UTF8, "application/x-www-form-urlencoded"));
+            try {
+                return await PostAsync(url, new StringContent(content, Encoding.UTF8, "application/x-www-form-urlencoded"));
+            } catch (Exception) {
+                return null;
+            }
         }
 
         private static async Task<String> PostAsync(String url, HttpContent content) {
-            HttpResponseMessage response    = await Client.PostAsync(url, content);
-            response.EnsureSuccessStatusCode();
-            //System.Diagnostics.Debug.Print(Convert.ToString(response.RequestMessage));
+            try {
+                HttpResponseMessage response    = await Client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                //System.Diagnostics.Debug.Print(Convert.ToString(response.RequestMessage));
 
-            return await response.Content.ReadAsStringAsync();
+                return await response.Content.ReadAsStringAsync();
+            } catch (Exception) {
+                return null;
+            }
         }
 
         public static async Task Get(String url, Action<String> callback) {
-            String getResponse = await GetAsync(url);
-            callback(getResponse);
+            callback(await GetAsync(url));
         }
 
         public static async Task Post(String url, KeyValuePair<String, String>[] data, Action<String> callback) {
-            String getResponse = await PostAsync(url, data);
-            callback(getResponse);
+            callback(await PostAsync(url, data));
         }
 
         public static async Task Post(String url, String data, Action<String> callback) {
-            String getResponse      = await PostAsync(url, data);
-            callback(getResponse);
+            callback(await PostAsync(url, data));
         }
     }
 }
