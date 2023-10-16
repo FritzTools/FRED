85d535c380c66269686e8f32c14e3628427aea14
Adrian Preuß <administrates@icloud.com>
2023-10-16 18:11:19 +0200

    Fix Boundary-Request

--- Source/Core/Fritzbox/Networking/Info/ConfigExport.cs
+++ Source/Core/Fritzbox/Networking/Info/ConfigExport.cs
@@ -1,27 +1,21 @@
 ﻿using FRED.Core.Data;
 using FRED.Core.Fritzbox.Networking.HTTP;
 using System;
+using System.Net.Http;
 
 namespace FRED.Core.Fritzbox.Networking.Info {
     public class ConfigExport {
         public static void Collect(AuthUser user, Action<String?> callback) {
-            /*MultipartFormDataContent multipart = new MultipartFormDataContent("WebKitFormBoundaryCtAy6cbSuOvDuO01");
-            multipart.Add(new StringContent("", Encoding.UTF8, "form-data"), "ConfigExport");
-            multipart.Add(new StringContent(password, Encoding.UTF8, "form-data"), "ImportExportPassword");
-            multipart.Add(new StringContent(user.SID, Encoding.UTF8, "form-data"), "sid");
-
-            multipart.Headers.ContentType.MediaType = "multipart/form-data";
-
-
-            System.Diagnostics.Debug.Print(Convert.ToString(multipart));*/
-
             String boundary = "CtAy6cbSuOvDuO01";
             String password = "test";
-            String request  = "------WebKitFormBoundary" + boundary  + "\r\nContent-Disposition: form-data; name=\"sid\"\r\n\r\n" + user.SID + "\r\n------WebKitFormBoundary" + boundary + "\r\nContent-Disposition: form-data; name=\"ImportExportPassword\"\r\n\r\n" + password + "\r\n------WebKitFormBoundary" + boundary + "\r\nContent-Disposition: form-data; name=\"ConfigExport\"\r\n\r\n\r\n------WebKitFormBoundary" + boundary + "--";
 
-            Request.Post("http://" + user.Endpoint + "/cgi-bin/firmwarecfg", request, delegate (String response) {
+            Request.Post("http://" + user.Endpoint + "/cgi-bin/firmwarecfg", new MultipartFormDataContent("----WebKitFormBoundary" + boundary) {
+                new FormDataContent("sid", user.SID),
+                new FormDataContent("ImportExportPassword", password),
+                new FormDataContent("ConfigExport", "")
+            }, delegate (String response) {
                 callback(response);
-            });
+            });            
         }
     }
 }
